f32af3a5089a5b998f6bc93ca48208f55cbd818a
matha <mathabaws@gmail.com>
2014-12-01 11:52:09 +0100

    Added JSON Parser class and DataLoader.

--- Sprint2/Code/src/com/example/seierfriendapp/MainActivity.java
+++ Sprint2/Code/src/com/example/seierfriendapp/MainActivity.java
@@ -10,10 +10,20 @@ import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 import com.example.fragments.LoginFragment;
+import com.example.services.DataCollectedListener;
+import com.example.services.JsonParser;
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONException;
+import org.json.JSONObject;
 
-public class MainActivity extends FragmentActivity{
+import java.util.ArrayList;
 
+public class MainActivity extends FragmentActivity implements DataCollectedListener{
+
+    JsonParser jsonParser;
     Button btnSignIn;
     Button btnRegister;
 
@@ -26,6 +36,9 @@ public class MainActivity extends FragmentActivity{
         bar.hide();
         setContentView(R.layout.login_layout);
 
+        jsonParser = new JsonParser(getApplicationContext());
+        jsonParser.setDataCollectedListener(this);
+
         LoginFragment firstFragment = new LoginFragment();
         getSupportFragmentManager().beginTransaction().add(R.id.fragment_container, firstFragment).commit();
 
@@ -34,8 +47,21 @@ public class MainActivity extends FragmentActivity{
             @Override
             public void onClick(View v) {
 
-                Intent i = new Intent(getApplication(), PointStatusActivity.class);
-                startActivity(i);
+                Resources res = getResources();
+                String url = String.format(res.getString(R.string.wsURI));
+
+                ArrayList<NameValuePair> header = new ArrayList<NameValuePair>();
+                header.add(new BasicNameValuePair("Authorization", "9a11e2b3-6ad8-4938-8219-74215228de98"));
+                Object jsonParameters[] = new Object[]{
+                        Uri.parse(url),
+                        "get",
+                        header
+                };
+
+                jsonParser.getData(jsonParameters);
+
+                /*Intent i = new Intent(getApplication(), PointStatusActivity.class);
+                startActivity(i);*/
             }
         });
 
@@ -49,5 +75,21 @@ public class MainActivity extends FragmentActivity{
                 startActivity(i);
             }
         });
+    }
+
+    @Override
+    public void DataCollected(boolean dataIsCollected, boolean errors, String errorMessage) {
+        if(dataIsCollected){
+            JSONObject jo = jsonParser.getJson();
+            try {
+                Toast.makeText(this, "MyActivity, data collected \n" +
+                        "Name: " + jo.getString("first_name").toString(), Toast.LENGTH_SHORT).show();
+            } catch (JSONException e) {
+                e.printStackTrace();
+                Toast.makeText(this, "MyActivity, data is not collected because \n"+e.toString(), Toast.LENGTH_SHORT).show();
+            }
+        }else{
+            Toast.makeText(this, "MyActivity, data is not collected because:\n\n"+errorMessage, Toast.LENGTH_SHORT).show();
+        }
     }
 }
